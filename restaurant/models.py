836f00f00292f4d7b77de31bc490a7ab294f8469
Youssefelshafey7 <youssef.afandy90@hotmail.com>
2025-11-15 21:05:35 +0200

    Updated models

--- restaurant/models.py
+++ restaurant/models.py
@@ -16,6 +16,6 @@ class Menu(models.Model):
     name = models.CharField(max_length=200) 
     price = models.IntegerField(null=False) 
     menu_item_description = models.TextField(max_length=1000, default='') 
-
+    inventory = models.IntegerField(null=False , default=0)
     def __str__(self):
         return self.name
