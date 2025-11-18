ea080c10427fa5a5628d2cfeb2933e88457bfb51
Youssefelshafey7 <youssef.afandy90@hotmail.com>
2025-11-19 01:01:42 +0200

    urls , settings

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -78,7 +78,11 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 
@@ -144,3 +148,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER={"USER_ID_FIELD":"username"}
