4be320a9185b48b401e23c863523def53034c499
Youssefelshafey7 <youssef.afandy90@hotmail.com>
2025-11-24 13:10:22 +0200

    modification at settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
     'LittleLemonAPI',
     # The settings for app updated for the Graded assessment
     'restaurant',
